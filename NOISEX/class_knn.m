5e0fcb28a6479a5483130469614c48f6bf28702d
Felipe Farias <epiles.farias@gmail.com>
2017-07-23 21:57:24 -0300

    novos .csvs em q linhas são frames

--- NOISEX/class_knn.m
+++ NOISEX/class_knn.m
@@ -1,88 +1,33 @@
-function [acc,tempo] = class_knn()
+function [acc,tempo] = class_knn
 
 %programa pra montar modelos de áudios usando knn 
 
-% limpando tela e variáveis anteriores
-
-%clear all; clc;
-
-% lendo as entradas (arquivos .csv de cada classe)
-
+%começa a contar o tempo de execução
 t_knn = tic;
 
-%{
+% lê o dataset
 cd .\csvs\
 
-babble = csvread('babble.csv');
-buccaneer1 = csvread('buccaneer1.csv');
-buccaneer2 = csvread('buccaneer2.csv');
-destroyerengine = csvread('destroyerengine.csv');
-destroyerops = csvread('destroyerops.csv');
-f16 = csvread('f16.csv');
-factory1 = csvread('factory1.csv');
-factory2 = csvread('factory2.csv');
-hfchannel = csvread('hfchannel.csv');
-leopard = csvread('leopard.csv');
-m109 = csvread('m109.csv');
-machinegun = csvread('machinegun.csv');
-pink = csvread('pink.csv');
-volvo = csvread('volvo.csv');
-white = csvread('white.csv');
+dataset = csvread('dataset.csv');
+%target = csvread('target.csv');
 
 cd ..
 
-% montando as classes
+%cria vetor de classes de cada frame usando a matriz 'dataset'
+classes = zeros(length(dataset),1);
 
-% Load the data and select features for classification
-n = 10;
-tam = ceil(length(babble)/n)-1;
+length(dataset)
 
-
-% monta o vetor 'treino' com os coeficientes mel-cepstrais de todas as matrizes e suas classes
-% correspondentes
-
-for i = 1:tam
-treino(i,:) = babble(n*i,:);
-classes(i) = 0;
-treino(i+tam,:) = buccaneer1(n*i,:);
-classes(i+tam) = 1;
-treino(i+2*tam,:) = buccaneer2(n*i,:);
-classes(i+2*tam) = 2;
-treino(i+3*tam,:) = destroyerengine(n*i,:);
-classes(i+3*tam) = 3;
-treino(i+4*tam,:) = destroyerops(n*i,:);
-classes(i+4*tam) = 4;
-treino(i+5*tam,:) = f16(n*i,:);
-classes(i+5*tam) = 5;
-treino(i+6*tam,:) = factory1(n*i,:);
-classes(i+6*tam) = 6;
-treino(i+7*tam,:) = factory2(n*i,:);
-classes(i+7*tam) = 7;
-treino(i+8*tam,:) = hfchannel(n*i,:);
-classes(i+8*tam) = 8;
-treino(i+9*tam,:) = leopard(n*i,:);
-classes(i+9*tam) = 9;
-treino(i+10*tam,:) = m109(n*i,:);
-classes(i+10*tam) = 10;
-treino(i+11*tam,:) = machinegun(n*i,:);
-classes(i+11*tam) = 11;
-treino(i+12*tam,:) = pink(n*i,:);
-classes(i+12*tam) = 12;
-treino(i+13*tam,:) = volvo(n*i,:);
-classes(i+13*tam) = 13;
-treino(i+14*tam,:) = white(n*i,:);
-classes(i+14*tam) = 14;
+for i = 1:length(dataset)
+    classes(i) = floor(i/(length(dataset)/15));
 end
+    
 
-%}
-
-% seleciona aleatoriamente partes de treino e teste dos dados
+% seleciona aleatoriamente partes de treino e teste dos dados usando função crossvalind
 [train, test] = crossvalind('holdOut',classes,0.25);
 
-
-
-% cria matriz 'dados' com os coeficientes usados como entrada do classificador
-dados = [ treino(:,1), treino(:,2)];
+% cria matriz 'dados' com os coeficientes usados como entrada do classificador (apenas os 2 primeiros, inicialmente)
+dados = [ dataset(:,1), dataset(:,2)];
 
 
 % incorpora as classes ao vetor de dados
@@ -100,17 +45,9 @@ for i = 1:length(dados)
     end
 end
 
-%plota os dados de treino
 
-gscatter(dtrain(:,1),dtrain(:,2),dtrain(:,3)); hold on;
-
-
-%classifica os dados de teste utilizando um classificador KNN
+%classifica os dados de teste utilizando um classificador KNN (apenas 1 vizinho)
 c = knnclassify(dtest, dtrain, dtrain(:,3));
-
-gscatter(dtest(:,1),dtest(:,2),c);
-
-hold off
 
 % verifica o desempenho do classificador
 cp = classperf(dtest(:,3));
